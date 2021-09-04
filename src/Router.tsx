030a5f61ee1102bf0d2f8cbd5cfaeb268e6f0a2b
Lucas Morais <lucascdemorais@gmail.com>
2021-09-03 21:07:28 -0300

    init

---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Routes from './routes'
+import { BrowserRouter } from 'react-router-dom'
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      
+    <Routes />
+  </BrowserRouter>
+  )
+   
+}
+export default Router
