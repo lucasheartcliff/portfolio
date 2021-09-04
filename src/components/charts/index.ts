23748c2256d7c90c7191a217e198e6fb69ef844e
Lucas Morais <lucas.morais@intelie.com.br>
2021-09-03 21:39:41 -0300

    adding annotations

---
+++ src/components/charts/index.ts
@@ -0,0 +1,3 @@
+// Editors -  https://apexcharts.com/react-chart-demos/pie-charts/simple-donut/#
+// Languages - https://apexcharts.com/react-chart-demos/column-charts/column-with-data-labels/
+// Code Activity  - https://apexcharts.com/react-chart-demos/line-charts/basic/
