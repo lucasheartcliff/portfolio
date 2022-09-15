cb91c143a9d7e4faec9ac3279e6ad7a0bab3f9a0
Lucas Morais <lucascdemorais@gmail.com>
2022-09-14 21:50:04 -0300

    adding antd with dark theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ThemeProvider } from './style/themes/theme-provider';
+require('antd/dist/antd.less')
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/style/themes/light/index.tsx
@@ -0,0 +1,5 @@
+import "./index.less";
+import React from "react"
+
+const LightTheme = () => <></>;
+export default LightTheme;
