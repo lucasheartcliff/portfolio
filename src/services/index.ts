38782929f0f85742dbf842a4b311b2fb32331eb6
Lucas Morais <lucascdemorais@gmail.com>
2022-10-17 02:55:10 -0300

    fixing cors on wakatime api request

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,49 +1,77 @@
+import { QueryKey, useQuery, UseQueryOptions } from "react-query";
+import { WAKATIME_LANGUAGES } from "../utils/url";
 
-import {QueryKey, useQuery, UseQueryOptions, UseQueryResult} from "react-query"
+import fj from "fetch-jsonp"
+
 const jsonHeaders: RequestInit = {
-    credentials: 'same-origin',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-}
-
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+  },
+};
 
 const apiFetch = (url: string) => {
-    const headers = jsonHeaders
-    
-    return {
-        get: () =>
-            fetch(url, {
-                method: 'GET',
-                ...headers
-            }),
-        post: (body: any) =>
-            fetch(url, {
-                method: 'POST',
-                ...headers,
-                body
-            }),
-        patch: (body: any) =>
-            fetch(url, {
-                method: 'PATCH',
-                ...headers,
-                body
-            }),
-        put: (body: any) =>
-            fetch(url, {
-                method: 'PUT',
-                ...headers,
-                body
-            }),
-        delete: () =>
-            fetch(url, {
-                method: 'DELETE',
-                ...headers
-            })
-    }
+  const headers = jsonHeaders;
+
+  return {
+    get: () =>
+      fj(url, {
+        method: "GET",
+        ...headers,
+      }),
+    post: (body: any) =>
+      fetch(url, {
+        method: "POST",
+        ...headers,
+        body,
+      }),
+    patch: (body: any) =>
+      fetch(url, {
+        method: "PATCH",
+        ...headers,
+        body,
+      }),
+    put: (body: any) =>
+      fetch(url, {
+        method: "PUT",
+        ...headers,
+        body,
+      }),
+    delete: () =>
+      fetch(url, {
+        method: "DELETE",
+        ...headers,
+      }),
+  };
+};
+
+const xhrFetch = (url: string, callback: any) => {
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = "json";
+  xhr.open("GET", url, true);
+  xhr.onload = () => {
+    callback(xhr.response);
+  };
+  xhr.send();
+};
+
+export function useGetRequest<TData, TError = any>(
+  url: string,
+  options?: Omit<
+    UseQueryOptions<TData, TError, TData, QueryKey>,
+    "queryKey" | "queryFn"
+  >
+) {
+  const fn = () => {
+    return apiFetch(url)
+      .get()
+      .then((r) => r.json());
+  };
+
+  return useQuery<TData, TError>(url, fn, options);
 }
 
-export function useGetRequest<TData, TError=any>(url: string, options?: Omit<UseQueryOptions<TData, TError, TData, QueryKey>, 'queryKey' | 'queryFn'>) {
-    const fn = ()=> apiFetch(url).get().then(r=> r.json())
-    return useQuery<TData,TError>(url, fn, options)
-}
+export const CACHE_TIME = 1000 * 60 * 60;
