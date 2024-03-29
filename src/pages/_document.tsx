dbf1145c8de325d42c0b231c4c24bc025f2047a1
Lucas Morais <lucascdemorais@gmail.com>
2024-03-29 16:56:13 -0300

    adding analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,11 @@
 /* eslint-disable no-underscore-dangle */
-import { GoogleAnalytics } from '@next/third-parties/google';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-
-import { getEnvProperties } from '@/utils';
 
 const i18nextConfig = require('../../next-i18next.config');
 // Need to create a custom _document because i18n support is not compatible with `next export`.
 class MyDocument extends Document {
   // eslint-disable-next-line class-methods-use-this
   render() {
-    const { googleAnalytics: gaId } = getEnvProperties();
-
     const currentLocale =
       this.props.__NEXT_DATA__.query.locale || i18nextConfig.i18n.defaultLocale;
 
@@ -22,7 +17,6 @@ class MyDocument extends Document {
             <Main />
             <NextScript />
           </body>
-          <GoogleAnalytics gaId={gaId} />
         </>
       </Html>
     );
